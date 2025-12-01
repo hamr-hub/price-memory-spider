940c80fa0fbf73bffecba8d0a04d18e1d93d6120
nichenghyx <1064042411@qq.com>
2025-12-01 18:11:14 +0800

    更新

--- tests/test_trend.py
+++ tests/test_trend.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from spider.main import init_db, create_product, get_conn, product_trend
 
 
